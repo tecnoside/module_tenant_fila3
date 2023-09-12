9e6d1051c6c124d31c852b64402b18f64744b892
marco76tv <marco.sottana@gmail.com>
2023-09-12 10:16:00 +0200

    up

--- Providers/TenantServiceProvider.php
+++ Providers/TenantServiceProvider.php
@@ -24,6 +24,7 @@ class TenantServiceProvider extends XotBaseServiceProvider
 
     public function bootCallback(): void
     {
+
         $this->mergeConfigs();
 
         if (Request::has('act') && 'migrate' === Request::input('act')) {
