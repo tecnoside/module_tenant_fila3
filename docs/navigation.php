9f131e955fd2653d3da78ae8599f71f6aeee0511
marco76tv <marco.sottana@gmail.com>
2023-11-16 16:17:28 +0100

    up

---
+++ docs/navigation.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'Getting Started' => [
+        'url' => 'docs/getting-started',
+        'children' => [
+            'Configurazioni progetti' => 'docs/project_configurations',
+            // 'Navigation' => 'docs/navigation',
+            // 'Algolia DocSearch' => 'docs/algolia-docsearch',
+            // 'Custom 404 Page' => 'docs/custom-404-page',
+        ],
+    ],
+    // 'Jigsaw Docs' => 'https://jigsaw.tighten.co/docs/installation',
+    'File di configurazione' => [
+        'url' => 'docs/configuration_files',
+        'children' => [
+            'file xra.php' => 'docs/xra',
+            // 'file metatag.php' => 'docs/metatag',
+            // 'file filesystems.php' => 'docs/filesystems',
+            // 'file database.php' => 'docs/database',
+            // 'files menu_builder' => 'docs/menu_builder',
+        ],
+    ],
+    'Links' => [
+        'url' => '#',
+        'children' => [
+            'links' => 'docs/links/links',
+        ],
+    ],
+];
---
+++ docs/source/docs/configuration_files.md
@@ -0,0 +1,17 @@
+---
+title: File di configurazione
+description: File di configurazione
+extends: _layouts.documentation
+section: content
+---
+
+# File di configurazione {#file-di-configurazione}
+
+In ogni cartella di configurazione, in base al dominio, possiamo inserire tutti i file che predispongono le caratteristiche che necessitano al progetto interessato, esempi:
+
++ il file che determina quale tema utilizzare, sia per l'amministrazione che per il frontend
++ il file che determina quale database utilizzare
++ il file che determina le caratteristiche dei metatag
++ il file che determina l'elenco dei modelli (e loro mappatura) utilizzati per il progetto interessato
++ i files che determinano la configurazione di menu creati tramite il menu builder
+
