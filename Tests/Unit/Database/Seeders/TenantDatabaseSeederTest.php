9c9a841c6fbfe6fffe554748c4b190d7c21abada
marco76tv <marco.sottana@gmail.com>
2024-01-19 11:03:46 +0100

    up

---
+++ Tests/Unit/Database/Seeders/TenantDatabaseSeederTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Modules\Tenant\Tests\Unit\Database\Seeders;
+
+use Modules\Tenant\Database\Seeders\TenantDatabaseSeeder;
+use Tests\TestCase;
+
+/**
+ * Class TenantDatabaseSeederTest.
+ *
+ * @covers \Modules\Tenant\Database\Seeders\TenantDatabaseSeeder
+ */
+final class TenantDatabaseSeederTest extends TestCase
+{
+    private TenantDatabaseSeeder $tenantDatabaseSeeder;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        /** @todo Correctly instantiate tested object to use it. */
+        $this->tenantDatabaseSeeder = new TenantDatabaseSeeder();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        unset($this->tenantDatabaseSeeder);
+    }
+
+    public function testRun(): void
+    {
+        /** @todo This test is incomplete. */
+        self::markTestIncomplete();
+    }
+}
