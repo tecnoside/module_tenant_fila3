d92fa85be887e087b984343b1282a6a3693e8fd5
marco <marco.sottana@gmail.com>
2024-05-31 09:32:37 +0200

    up

--- Models/Domain.php
+++ Models/Domain.php
@@ -8,6 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 use Modules\Tenant\Actions\Domains\GetDomainsArrayAction;
 use Sushi\Sushi;
 
+/**
+ * 
+ *
+ * @property int|null $id
+ * @property string|null $name
+ * @method static \Illuminate\Database\Eloquent\Builder|Domain newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Domain newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Domain query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Domain whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Domain whereName($value)
+ * @mixin \Eloquent
+ */
 class Domain extends Model
 {
     use Sushi;
