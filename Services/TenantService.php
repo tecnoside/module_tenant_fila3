4f638c0c1fc357484631ca0a0252db86214fd7a4
marco <marco.sottana@gmail.com>
2024-07-01 10:24:35 +0200

    up

--- Services/TenantService.php
+++ Services/TenantService.php
@@ -165,14 +165,14 @@ class TenantService
         // -- ogni modulo ha la sua connessione separata
         // -- replicazione liveuser con lu.. tenere lu anche in database
         if ($key === 'database') {
-            $default=Arr::get($extra_conf,'default',null);
-            if($default==null){
-                $default=Arr::get($original_conf,'default',null);
+            $default = Arr::get($extra_conf, 'default', null);
+            if ($default == null) {
+                $default = Arr::get($original_conf, 'default', null);
             }
-            if($default==null){
-                $default='mysql';
+            if ($default == null) {
+                $default = 'mysql';
             }
-            
+
             /**
              * @var Collection<\Nwidart\Modules\Module>
              */
