c5d9c6f2f81dab444164ce72a5c7511ad0b5b545
marco76tv <marco.sottana@gmail.com>
2024-08-06 09:05:28 +0200

    .

--- Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('tenant::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('tenant.name') !!}
-    </p>
-@endsection
