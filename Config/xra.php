5ca2d2655176f2477cb65719d748317618cbb079
marco76tv <marco.sottana@gmail.com>
2024-08-19 17:20:36 +0200

    up

--- Config/xra.php
+++ Config/xra.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 return [
     'adm_theme' => 'Clever',
     'enable_ads' => '1',
-    'main_module' => 'PFed',
+    'main_module' => 'Tenant',
     'primary_lang' => 'it',
     'pub_theme' => 'One',
 ];
