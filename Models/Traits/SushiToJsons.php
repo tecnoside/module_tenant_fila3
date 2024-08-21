f06cf73fd2ed66db89b7bcadf166d9f71e1e47de
marco76tv <marco.sottana@gmail.com>
2024-08-21 14:40:13 +0200

    up

---
+++ Models/Traits/SushiToJsons.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * @see https://dev.to/hasanmn/automatically-update-createdby-and-updatedby-in-laravel-using-bootable-traits-28g9.
+ */
+
+declare(strict_types=1);
+
+namespace Modules\Tenant\Models\Traits;
+
+use Illuminate\Support\Facades\File;
+use Modules\Tenant\Services\TenantService;
+
+trait SushiToJsons
+{
+    public function getSushiRows(): array
+    {
+        $tbl = $this->getTable();
+        $path = TenantService::filePath('database/content/'.$tbl);
+        $files = File::glob($path.'/*.json');
+        $rows = [];
+        foreach ($files as $id => $file) {
+            $json = File::json($file);
+            $item = [];
+            foreach ($this->schema as $name => $type) {
+                $value = $json[$name] ?? null;
+                if (is_array($value)) {
+                    $value = json_encode($value, JSON_PRETTY_PRINT);
+                }
+                $item[$name] = $value;
+            }
+            $rows[] = $item;
+        }
+
+        return $rows;
+    }
+
+    public function getJsonFile(): string
+    {
+        $tbl = $this->getTable();
+        $id = $this->getKey();
+        $file = TenantService::filePath('database/content/'.$tbl.'/'.$id.'.json');
+
+        return $file;
+    }
+
+    /**
+     * bootUpdater function.
+     */
+    protected static function bootSushiToJsons(): void
+    {
+        /*
+         * During a model create Eloquent will also update the updated_at field so
+         * need to have the updated_by field here as well.
+         */
+        static::creating(
+            function ($model): void {
+                $model->id = $model->max('id') + 1;
+                $model->updated_at = now();
+                $model->updated_by = authId();
+                $model->created_at = now();
+                $model->created_by = authId();
+                $data = $model->toArray();
+                $item = [];
+                foreach ($model->schema as $name => $type) {
+                    $value = $data[$name] ?? null;
+                    $item[$name] = $value;
+                }
+                $content = json_encode($item, JSON_PRETTY_PRINT);
+                $file = $model->getJsonFile();
+                File::put($file, $content);
+            }
+        );
+        /*
+         * updating.
+         */
+        static::updating(
+            function ($model): void {
+                $file = $model->getJsonFile();
+                $model->updated_at = now();
+                $model->updated_by = authId();
+                $content = $model->toJson(JSON_PRETTY_PRINT);
+                File::put($file, $content);
+            }
+        );
+        // -------------------------------------------------------------------------------------
+        /*
+         * Deleting a model is slightly different than creating or deleting.
+         * For deletes we need to save the model first with the deleted_by field
+        */
+
+        static::deleting(
+            function ($model): void {
+                unlink($model->getJsonFile());
+            }
+        );
+
+        // ----------------------
+    }
+
+    // end function boot
+}// end trait Updater
