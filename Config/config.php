eddef46d2cb847a99b34f35cf458dc4fcc2277c8
marco <marco.sottana@gmail.com>
2024-10-08 17:07:49 +0200

    up

--- Config/config.php
+++ Config/config.php
@@ -1,18 +1,20 @@
-<?php return array (
-  'name' => 'Tenant',
-  'navigation_sort' => 1,
-  'icon' => 'heroicon-o-cog',
-  'id' => 10,
-  'description' => '',
-  'status' => true,
-  'priority' => 2,
-  'colors' => 
-  array (
-    0 => 
-    array (
-      'key' => 'primary',
-      'value' => 'blue',
-      'color' => NULL,
-    ),
-  ),
-);
+<?php
+
+declare(strict_types=1);
+
+return [
+    'name' => 'Tenant',
+    'navigation_sort' => 1,
+    'icon' => 'heroicon-o-cog',
+    'id' => 10,
+    'description' => '',
+    'status' => true,
+    'priority' => 2,
+    'colors' => [
+        0 => [
+            'key' => 'primary',
+            'value' => 'blue',
+            'color' => '#8c3e3e',
+        ],
+    ],
+];
