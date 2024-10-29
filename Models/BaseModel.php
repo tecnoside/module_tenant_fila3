bc02740c6018b6e639851e1c7c06be3809b21f19
marco76tv <marco.sottana@gmail.com>
2024-10-29 12:20:12 +0100

    up

--- Models/BaseModel.php
+++ Models/BaseModel.php
@@ -63,7 +63,7 @@ abstract class BaseModel extends Model
     }
 
     /** @return array<string, string> */
-    protected function casts(): array
+    public function casts(): array
     {
         return [
             'id' => 'string',
